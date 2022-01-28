c888fb5b399488dfd0178248998aef36af42bd70
Filip Gregor <filip.gregor98@gmail.com>
2022-01-28 10:33:12 +0100

    Added external linkage to writeln function, which is implicitly declared

--- src/Codegen/Program.cpp
+++ src/Codegen/Program.cpp
@@ -1,10 +1,34 @@
+#include "llvm/ADT/APFloat.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Verifier.h"
+
 #include "include/Codegen.h"
 #include "include/Nodes/Program.h"
+#include "include/Utility.h"
 
 void Codegen::Visit(const Program &program)
 {
+    Function writeln("writeln", Type::INTEGER, {{"val", Type::INTEGER}});
+    writeln.Accept(*this);
+    llvm::Function::Create(
+        llvm::FunctionType::get(llvm::Type::getInt32Ty(context) ,false), 
+        llvm::Function::ExternalLinkage, 
+        "writeln",
+        module);
+
     for(const auto &f : program.functions) {
         f.second.Accept(*this);
     }
     module.print(llvm::errs(), nullptr);
+
+
+
 }
